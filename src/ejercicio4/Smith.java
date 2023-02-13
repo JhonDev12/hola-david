12b82ce8dd52cbc8950b551553f3a06f5422b081
smith <jhonsenapro@gmail.com>
2023-02-13 14:14:04 -0500

    hola smith

--- src/ejercicio4/Smith.java
+++ src/ejercicio4/Smith.java
@@ -2,13 +2,15 @@ package ejercicio4;
 
 public class Smith {
 
-	public static void main(String[] args) {
+	public static void main(String[] args, Object System) {
 		// TODO Auto-generated method stub
 
 		
 		
 		System.out.println("hola soy smith");
 		System.out.println("toca cajon");
+		System.out.println("hola vaca ");
+		
 	}
 
 }
